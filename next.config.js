ecbe3d74635a1a0a6ceffb83d205f208f29ec0e9
wverdan84-eng <wverdan84@gmail.com>
2026-01-17 14:44:09 -0300

    Refactor next.config.js to simplify config options

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // Macete: Ignora erros de tipo para o build passar direto
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Macete: Ignora avisos de formatação durante o deploy
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 }
-
 module.exports = nextConfig
