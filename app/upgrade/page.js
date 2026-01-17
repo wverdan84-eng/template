9fd672e70f50761e130aea908769d4330d89b965
wverdan84-eng <wverdan84@gmail.com>
2026-01-17 14:30:55 -0300

    Delete app directory

--- app/upgrade/page.js
+++
@@ -1,9 +0,0 @@
-export default function Upgrade() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Plano PRO</h1>
-      <p>Acesso completo por apenas R$29/mês.</p>
-      <a href="#">Comprar agora</a>
-    </main>
-  );
-}
