dd751fea5f915a5e21b52a4f216909c6b8e36398
wverdan84-eng <wverdan84@gmail.com>
2026-01-17 11:31:01 -0300

    Criar app/page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>SaaS Template</h1>
+      <p>Seu SaaS está online 🚀</p>
+      <a href="/dashboard">Acessar</a>
+    </main>
+  );
+}
