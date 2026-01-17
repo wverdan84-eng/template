9080dc3e6cbd4099c16082726d13c395235d1ce1
wverdan84-eng <wverdan84@gmail.com>
2026-01-17 11:32:31 -0300

    Criar app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "SaaS Template",
+  description: "Template SaaS Next.js"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
