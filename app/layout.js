9afaef5e1d563d88257ae5b61d3c2d9da33051f4
wverdan84-eng <wverdan84@gmail.com>
2026-01-17 11:49:17 -0300

    Atualizar o layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,15 @@
-import { SessionProvider } from "next-auth/react";
+import { Providers } from "./providers";
+
+export const metadata = {
+  title: "SaaS Template",
+  description: "Template SaaS Next.js"
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>
-        <SessionProvider>{children}</SessionProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
